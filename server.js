c35daab7f10b9fd3f2188f50f0c307f90897e91b
rajsahilcoder <rajsahilcoder@gmail.com>
2023-06-01 11:24:41 +0530

    Calculator_app

--- server.js
+++ server.js
@@ -3,26 +3,30 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = '/calculator';
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.status(200).send('Hello world!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    const result = first + second;
+    res.status(200).json({ result });
 });
 
-
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    const result = first - second;
+    res.status(200).json({ result });
 });
 
 app.use(baseUrl, baseRouter);
+
 app.listen(PORT, () => {
     console.log("Server running at PORT", PORT);
 });
